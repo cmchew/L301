e757c6f78ff18e03bcaa3df4ba0df96ee41d65a1
cmchew <write_on@hotmail.com>
2020-01-08 14:29:34 -0500

    L301

---
+++ src/main/java/com/example/demo/L301Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L301Application.class, args);
+    }
+
+}
